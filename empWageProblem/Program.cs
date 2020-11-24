cdd20686116d196c8f0cc5751b50df5565d3e14c
“teji1998” <tejasvinirpk@gmail.com>
2020-11-24 22:43:02 +0530

    Added:Welcome message

---
+++ empWageProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace empWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
