0c153099ca76417df65d6c0abb8144ab5d703b53
“teji1998” <tejasvinirpk@gmail.com>
2020-11-25 10:34:44 +0530

    Added:Code to calculate daily wage

--- empWageProblem/Program.cs
+++ empWageProblem/Program.cs
@@ -8,6 +8,11 @@ namespace empWageProblem
         {
             //constant
             const int IS_PRESENT = 1;
+            const int HOURLY_WAGE = 20;
+
+            //variables
+            int employeeHours = 0;
+            int employeeWage = 0;
 
             //Welcome message
             Console.WriteLine("Welcome to employee wage computation problem!");
@@ -19,12 +24,18 @@ namespace empWageProblem
             // using if else loop to check if employee is present or not
             if (attendance == IS_PRESENT)
             {
+                employeeHours = 8;;
                 Console.WriteLine("The employee is present!");
             }
             else
             {
+                employeeHours = 0;
                 Console.WriteLine("The employee is not present");
             }
+
+            employeeWage = HOURLY_WAGE * employeeHours;
+            Console.WriteLine("The number of working hours of employee is : " + employeeHours);
+            Console.WriteLine("The daily wage of the employee is : " + employeeWage);
 
         }
     }
