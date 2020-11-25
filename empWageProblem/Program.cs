3ae6022f86a159f9006890c10eb5cd8fe7f3d039
“teji1998” <tejasvinirpk@gmail.com>
2020-11-25 09:09:00 +0530

    Modified:Added comments

--- empWageProblem/Program.cs
+++ empWageProblem/Program.cs
@@ -6,10 +6,17 @@ namespace empWageProblem
     {
         static void Main(string[] args)
         {
+            //constant
             const int IS_PRESENT = 1;
+
+            //Welcome message
             Console.WriteLine("Welcome to employee wage computation problem!");
+
+            //Random class reference
             Random random = new Random();
             int attendance = random.Next(0, 2);
+
+            // using if else loop to check if employee is present or not
             if (attendance == IS_PRESENT)
             {
                 Console.WriteLine("The employee is present!");
