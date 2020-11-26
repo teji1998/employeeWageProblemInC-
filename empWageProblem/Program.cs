ffb336d13b0880dc6a68f0fe169e14097e783d6e
“teji1998” <tejasvinirpk@gmail.com>
2020-11-27 04:49:53 +0530

    Added:Code to calculate part-time employee wage

--- empWageProblem/Program.cs
+++ empWageProblem/Program.cs
@@ -7,9 +7,11 @@ namespace empWageProblem
         static void Main(string[] args)
         {
             //constant
-            const int IS_PRESENT = 1;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int HOURLY_WAGE = 20;
             const int FULL_TIME_EMPLOYEE_HOURS = 8;
+            const int PART_TIME_EMPLOYEE_HOURS = 4;
             const int ABSENT_EMPLOYEE_HOURS = 0;
 
             //variables
@@ -21,18 +23,23 @@ namespace empWageProblem
 
             //Random class reference
             Random random = new Random();
-            int attendance = random.Next(0, 2);
+            int attendance = random.Next(0, 3);
 
-            // using if else loop to check if employee is present or not
-            if (attendance == IS_PRESENT)
+            //To check working hours for part-time or full-time employee
+            if (attendance == IS_FULL_TIME)
             {
                 employeeHours = FULL_TIME_EMPLOYEE_HOURS;
-                Console.WriteLine("The employee is present!");
+                Console.WriteLine("The full-time employee is present!");
+            }
+            else if (attendance == IS_PART_TIME)
+            {
+                employeeHours = PART_TIME_EMPLOYEE_HOURS;
+                Console.WriteLine("The part-time employee is present!");
             }
             else
             {
                 employeeHours = ABSENT_EMPLOYEE_HOURS;
-                Console.WriteLine("The employee is absent");
+                Console.WriteLine("The employee is absent!");
             }
 
             employeeWage = HOURLY_WAGE * employeeHours;
